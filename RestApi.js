1f6f0fed97477e2e4521d8b532ed3a9c79517142
hanmo <dlgksah@gmail.com>
2017-07-07 09:59:52 +0900

    mysql connect

--- RestApi.js
+++ RestApi.js
@@ -6,37 +6,51 @@
 //DELETE는 "삭제하다"
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var sql = require('mysql');
 
-var app = express();
-
-var config = {
+var config = sql.createConnection({
     server : 'localhost',
     database : 'node_mysql',
     user : 'root',
-    password : '1',
-    port : 3000
-};
+    password : '1'
+});
+
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(bodyParser.json());//request body객체 안의 데이터를 json형식으로 인코딩
 
 config.connect((err) => {  
 if(!err) {  
     console.log("Database is connected ... \n\n");    
-    return res.status(200).json({error: 'DB Connected!!'});
 } 
 else {  
     console.log("Error connecting database ... \n\n");    
-    return res.status(400).json({error: 'DB NOT Connect!! ERROR!!!'});
 }  
 });
 
-
-app.get('/users', (req, res) => {
-
-    return res.json(users);
-
-})
+app.get("/",function(request,response){  
+config.query('use node_mysql');
+config.query('SELECT * from data', function(err, rows, fields) {  
+config.end();  
+  if (!err){  
+    response.send(rows);   
+    console.log('The solution is: ', rows);  
+  }  
+  else  
+    console.log('Error while performing Query.');  
+  });  
+});  
 
 
 app.listen(3000, () => {//서버가 클라이언트 요청 대기상태
-  console.log('REST api listening on port 3000!');//listen이 완료되면 실행되는 콜백함수
+    console.log('REST api listening on port 3000!');//listen이 완료되면 실행되는 콜백함수
+
+    /*require('./model').sequelize.sync({force: true}).then(() => {
+        //model(sequelize객체)를 가져와서 sync함수를 실행
+        //force(boolean data)를 true로 주어 실행이되면 무조건 테이블을 생성하도록 함
+        console.log('Database Sync!!');
+    });*/
 });
