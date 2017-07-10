4c7cb5f5a2ab09a9879c8f2a0a285800a73982ca
hanmo <dlgksah@gmail.com>
2017-07-10 09:49:32 +0900

    add input data of time to insert mysql column

--- RestApi.js
+++ RestApi.js
@@ -9,9 +9,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var sql = require('mysql');
 var Serialport = require('serialport');
-var value;
+require('date-utils');//date format을 위한 모듈 사용
 
-var port = new Serialport('/dev/ttyACM1',{
+
+var port = new Serialport('/dev/ttyACM0',{
     baudrate: 9600,
     parser: Serialport.parsers.readline('\n')
 });
@@ -32,11 +33,14 @@ app.use(bodyParser.json());//request body객체 안의 데이터를 json형식�
 
 port.on('open', () => {
     console.log('Serial Port open!!');
-    
-    port.on('data', (data) => {
-        console.log('data : ' + data);
         
-        insert(data);
+    port.on('data', (data, time) => {
+        time = new Date();
+        var datetime = time.toFormat('YYYY-MM-DD HH24:MI:SS');//table 컬럼에 맞는 값으로 변환
+
+        console.log('data : ' + data + 'time : ' + datetime);
+        
+        insert(data, datetime);//insert
                 
         
     });
@@ -54,19 +58,21 @@ else {
 });
 
 
-function insert(data) {
+function insert(data, time) {
     app.get("/hanmo", (req, res) => {
 
+        console.log('데이터 인서트!!!!! : '+data+ ' : ' +time);
+        
         //var data1 ="'value','2017-07-07 11:55:21'";
         var data2 = "'2017-07-07 11:55:21'";
         config.query('use node_mysql');
-        config.query('call insertdata('+data+','+data2+');', (err, rows) => {
+        config.query('call insertdata('+data+','+"'"+time+"'"+');', (err, rows) => {
             //현재 한번의 페이지에 한번의 insert가 이루어진다
             //내가 원하는 것은 데이터가 들어올경우에 항상 insert를 원한다...
         //config.end();
   
   
-                
+        
         if (!err){  
             console.log('complete!!!', rows);  
         }  
